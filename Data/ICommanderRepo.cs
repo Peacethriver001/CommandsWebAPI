2550863f8a4d72842a7b90736675ef72e47a9c35
Peace Bakare <Peace.Bakare@sterling.ng>
2022-03-28 11:47:10 +0100

    Updated the mcoks for the commander repository

--- Data/ICommanderRepo.cs
+++ Data/ICommanderRepo.cs
@@ -5,8 +5,10 @@ namespace CommanderWebAPI.Data
 {
     public interface ICommanderRepo
     {
+        //Interface to get all the commands as a list.
         IEnumerable<Command> GetAllCommands();
 
+        //Call this interface to get individual command. You'd need to know the id of the command to use this interface.
         Command GetCommandById(int id); 
     }
 }
--- Data/Implementation/MockCommanderRepo.cs
+++ Data/Implementation/MockCommanderRepo.cs
@@ -9,16 +9,16 @@ namespace CommanderWebAPI.Data.Implementation
         {
             var commands = new List<Command>
             {
-                new Command {Id = 0, HowTo = "Boil an Egg", Line = "Boil water", Platform = "Kettle & Pan"},
-                new Command {Id = 1, HowTo = "Break a nut", Line = "Break gently", Platform = "Knife and hard surface"},
-                new Command {Id = 2, HowTo = "Dance to a song", Line = "Play favorite music", Platform = "Music from phone or PC"}
+                new Command {Id = 0, HowTo = "Initiate git in a folder", Line = "git init", Platform = "Windows PC/Linux using git bash"},
+                new Command {Id = 1, HowTo = "Clone a repository to PC", Line = "git clone URL", Platform = "Windows PC/Linux using git bash"},
+                new Command {Id = 2, HowTo = "Add a new file to git working directory", Line = "git add FILE_NAME", Platform = "Windows PC/Linux using git bash"}
             };
             return commands;
         }
 
         public Command GetCommandById(int id)
         {
-            return new Command { Id = 0, HowTo = "Boil an Egg", Line = "Boil water", Platform = "Kettle & Pan"};
+            return new Command { Id = 0, HowTo = "Initiate git in a folder", Line = "git init", Platform = "Windows PC/Linux using git bash" };
         }
     }
 }
